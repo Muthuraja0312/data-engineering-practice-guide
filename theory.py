b705b84980675920fc7b3a4c72108769c7b36705
Kaushi Murugan <110972110+KaushiMurugan@users.noreply.github.com>
2026-09-12 10:13:29 +0530

    Update theory.py

--- theory.py
+++ theory.py
@@ -3,3 +3,10 @@
 supports redefining keys without rewriting data, and adapts to skew and changing query patterns. It’s enabled with CLUSTER BY during table creation, and 
 Databricks manages optimization automatically. 
 Compared to partitioning, it’s more flexible and efficient for high‑cardinality, fast‑growing, or streaming datasets."
+=========================================================================================================================
+Partition Pruning -> Skip folders
+Predicate Pushdown -> Skip rows
+Column Pruning -> Skip unnecessary columns in Spark plan
+Projection Pushdown -> Skip unnecessary columns at storage read
+
+Partition pruning reduces partitions scanned, predicate pushdown reduces rows scanned, column pruning removes unused columns from Spark's execution plan, and projection pushdown ensures only those required columns are physically read from the source.
